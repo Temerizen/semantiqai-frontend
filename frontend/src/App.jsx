e2089ae733cee6a31585cf29709867f603837b2b
Temerizen <macaustin2004@gmail.com>
2026-03-22 16:40:59 -0700

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>🚀 SemantiqAI is LIVE</h1>
-      <p>Your system is successfully deployed.</p>
-    </div>
-  )
-}
