700ec1659c962b401ca46bf61f7793ab7f1b6a59
Temerizen <macaustin2004@gmail.com>
2026-03-22 18:43:21 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-﻿import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import AppLayout from './layouts/AppLayout'
-import HomePage from './pages/HomePage'
-import DashboardPage from './pages/DashboardPage'
-import LearningPage from './pages/LearningPage'
-import CognitivePage from './pages/CognitivePage'
-import ExecutionPage from './pages/ExecutionPage'
-import SimulationPage from './pages/SimulationPage'
-import CreationPage from './pages/CreationPage'
-import FounderPage from './pages/FounderPage'
-import PricingPage from './pages/PricingPage'
-import LoginPage from './pages/LoginPage'
-import NotFoundPage from './pages/NotFoundPage'
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import DashboardPage from "./pages/DashboardPage"
+import HomePage from "./pages/HomePage"
+import PricingPage from "./pages/PricingPage"
+import LoginPage from "./pages/LoginPage"
+import FounderPage from "./pages/FounderPage"
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="dashboard" element={<DashboardPage />} />
-        <Route path="learning" element={<LearningPage />} />
-        <Route path="cognitive" element={<CognitivePage />} />
-        <Route path="execution" element={<ExecutionPage />} />
-        <Route path="simulation" element={<SimulationPage />} />
-        <Route path="creation" element={<CreationPage />} />
-        <Route path="founder" element={<FounderPage />} />
-        <Route path="pricing" element={<PricingPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/founder" element={<FounderPage />} />
     </Routes>
   )
 }
