7220a9739db294a444dc1cc9bb1a767c77a46d4e
Temerizen <macaustin2004@gmail.com>
2026-03-22 18:59:55 -0700

    Update routes.js

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,12 +1,11 @@
-﻿export const routeLabels = {
+export const routeLabels = {
   '/': 'Home',
   '/dashboard': 'Dashboard',
-  '/learning': 'AI School',
-  '/cognitive': 'Cognitive Lab',
+  '/school': 'AI School',
+  '/cognitive-lab': 'Cognitive Lab',
   '/execution': 'Execution Engine',
   '/simulation': 'Reality Simulation',
   '/creation': 'Infinite Creation',
   '/pricing': 'Pricing',
-  '/login': 'Sign In',
   '/founder': 'Founder Control'
 }
