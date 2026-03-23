f0ec1ed1a879c6a4cc63e54b6d3a3fc65109af1c
Temerizen <macaustin2004@gmail.com>
2026-03-22 18:56:42 -0700

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,21 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
-export default function Card({ title, desc }) {
-  return (
-    <div className="card">
+export default function Card({ title, desc, to }) {
+  const content = (
+    <>
       <h3>{title}</h3>
       <p>{desc}</p>
-    </div>
+    </>
   )
+
+  if (to) {
+    return (
+      <Link to={to} className="card" style={{ textDecoration: "none", color: "inherit", display: "block" }}>
+        {content}
+      </Link>
+    )
+  }
+
+  return <div className="card">{content}</div>
 }
